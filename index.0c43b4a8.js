c746c41a53d6cd47ecd56b92eb1baf95076abf7d
MacBook Pro 13 <macbookpro13@MacBook-Pro-MacBook.local>
2024-06-29 12:05:15 +0300

    make build

---
+++ index.0c43b4a8.js
@@ -0,0 +1,2 @@
+var t=document.getElementsByTagName("div")[0],e=document.getElementsByTagName("img")[0];t.addEventListener("click",function(n){var i=n.clientX-t.offsetLeft,a=n.clientY-t.offsetTop,c=t.clientWidth-e.clientWidth,l=t.clientHeight-e.clientHeight;e.style.left="".concat(Math.max(0,Math.min(i,c)),"px"),e.style.top="".concat(Math.max(0,Math.min(a,l)),"px")});
+//# sourceMappingURL=index.0c43b4a8.js.map
